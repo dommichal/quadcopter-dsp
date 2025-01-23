02bf3fcd3c78e68dc89d350d3dcd6ae3f2c1f3c3
Dominik Michalczyk <dmichalczyk73@gmail.com>
2025-01-23 21:28:22 +0100

    add USB support and bootloader configuration for STM32F103

--- firmware/functions.cmake
+++ firmware/functions.cmake
@@ -1,16 +1,4 @@
-function(target_stlink_flash CMAKE_PROJECT_NAME)
-    add_custom_target(flash_${CMAKE_PROJECT_NAME}
-        openocd
-        -f ${CMAKE_SOURCE_DIR}/Devices/Debug/openocd.cfg
-        -c "program ${BUILD_DIR}/${CMAKE_PROJECT_NAME}.elf verify reset exit"
-        COMMENT "Flashing ${CMAKE_PROJECT_NAME} with OpenOCD"
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        )
-    add_dependencies(flash flash_${CMAKE_PROJECT_NAME})
-endfunction(target_stlink_flash)
-
-
-function(target_stlink_flash TARGET)
+function(target_stlink_flash_app TARGET)
     add_custom_target(${TARGET}.flash_stlink
             openocd 
             -f ${CMAKE_SOURCE_DIR}/Devices/Debug/openocd.cfg
@@ -18,4 +6,13 @@ function(target_stlink_flash TARGET)
             DEPENDS ${TARGET}
             COMMENT "Flashing target hardware"
             WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
-endfunction(target_stlink_flash)
+endfunction(target_stlink_flash_app)
+
+function(target_stlink_flash_bootloader TARGET)
+    add_custom_target(bootloader.flash_stlink
+            openocd 
+            -f ${CMAKE_SOURCE_DIR}/Devices/Debug/openocd.cfg
+            -c "program ${CMAKE_SOURCE_DIR}/build/MSD_Bootloader-prefix/src/MSD_Bootloader-build/${TARGET}.elf reset exit"
+            COMMENT "Flashing target hardware"
+            WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+endfunction(target_stlink_flash_bootloader)
